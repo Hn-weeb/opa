9494da0ab9d4ffb057f73444aace1933aed1a7dd
Hn-weeb <80450767+Hn-weeb@users.noreply.github.com>
2022-01-10 23:13:15 +0300

    hn1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("ASD!");
-        System.out.println("Line");
+        System.out.println("Lie");
     }
 }
