f9f28e39053098711589f0218d83523ca5908c0d
Hn-weeb <80450767+Hn-weeb@users.noreply.github.com>
2022-01-10 23:17:42 +0300

    hn1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ASD!");
         System.out.println("Lie");
+        System.out.println("qwee");
+        System.out.println("Lewie");
     }
 }
